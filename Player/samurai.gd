24770782fdd0d1de79fb6e8bb14cfde5a494503f
Tadd Trumbull II <taddtrumbullii@gmail.com>
2025-01-07 10:43:51 -0500

    some scene changes

--- Player/samurai.gd
+++ Player/samurai.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 200.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -350.0
 @export var health = 100
 @onready var anim = get_node("AnimatedSprite2D")
 @export var is_alive = true
