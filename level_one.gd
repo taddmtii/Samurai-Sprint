6d4be761a01519333a0d6ac2ce572628db5cdc9d
Tadd Trumbull II <taddtrumbullii@gmail.com>
2025-01-19 09:48:03 -0500

    more level design

--- level_one.gd
+++ level_one.gd
@@ -7,3 +7,7 @@ extends Node2D
 	#var skeleton_instance = skeleton_scene.instantiate()
 	#skeleton_instance.position = Vector2(randi_range(20, 500), -20)
 	#add_child(skeleton_instance)
+
+func _on_finish_sign_area_entered(area: Area2D) -> void:
+	if area.name == "Samurai":
+		get_tree().change_scene_to_file("res://level_completed.tscn")
