47c28a8baa5066cf398c943f03acd54d4c57b052
Tadd Trumbull II <taddtrumbullii@gmail.com>
2025-01-19 09:31:37 -0500

    more level design

--- level_one.gd
+++ level_one.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
-var skeleton_scene = preload("res://Skeleton.tscn")
+#var skeleton_scene = preload("res://Skeleton.tscn")
 
 #spawn skeleton on timer timeout
-func _on_timer_timeout() -> void:
-	var skeleton_instance = skeleton_scene.instantiate()
-	skeleton_instance.position = Vector2(randi_range(20, 500), -20)
-	add_child(skeleton_instance)
+#func _on_timer_timeout() -> void:
+	#var skeleton_instance = skeleton_scene.instantiate()
+	#skeleton_instance.position = Vector2(randi_range(20, 500), -20)
+	#add_child(skeleton_instance)
